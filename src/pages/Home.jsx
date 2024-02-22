71a51db198edb1e55683cf4c79be07595e65643d
yaza <wayan13yasa@gmail.com>
2024-02-23 03:31:00 +0800

    progress 90%

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,58 @@ import Header from "../components/Header.jsx";
 import Sidebar from "../components/Sidebar.jsx";
 import JobInput from "../components/JobInput.jsx";
 import QueueMonitor from "../components/QueueMonitor.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 export default function Home() {
+    const [socket, setSocket] = useState(null);
+    const [rows, setRows] = useState([])
+    const [count, setCount] = useState({
+        pending : 0,
+        done : 0,
+        failed : 0
+    })
+
+    useEffect(() => {
+        const newSocket = new WebSocket(import.meta.env.VITE_WSS_URL);
+        setSocket(newSocket);
+
+        newSocket.onopen = () => {
+            console.log("Client connected!");
+        }
+
+        newSocket.onmessage = (event) => {
+            let receive =  JSON.parse(event.data)
+            console.log("Received message:",receive);
+            if (receive.event == "dashboard_count") {
+                setCount({
+                    pending: receive.data.Pending,
+                    done: receive.data.Done,
+                    failed: receive.data.Failed
+                })
+            }
+
+            if (receive.event == "update_table") {
+                setRows(receive.data.rows)
+            }
+
+        }
+
+        newSocket.onclose = () => {
+            console.log("Client disconnected!");
+        }
+
+        return () => newSocket.close();
+    }, [])
+
+
+    const excePendingJob = () => {
+        axios.get("/api/v1/queue/check")
+    }
+
+    const rollBackFailedJob = () => {
+        axios.get("/api/v1/queue/rollback")
+    }
+
 
    return (
        <>
@@ -14,23 +65,38 @@ export default function Home() {
                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                        <div className="bg-white p-6 rounded-lg shadow-md">
                            <h2 className="text-xl font-semibold text-gray-800 mb-4">PENDING</h2>
-                           <p className="text-3xl font-bold text-yellow-500">1,000</p>
+                           <p className="text-3xl font-bold text-yellow-500">{count.pending}</p>
                        </div>
                        <div className="bg-white p-6 rounded-lg shadow-md">
                            <h2 className="text-xl font-semibold text-gray-800 mb-4">DONE</h2>
-                           <p className="text-3xl font-bold text-green-600">800</p>
+                           <p className="text-3xl font-bold text-green-600">{count.done}</p>
                        </div>
                        <div className="bg-white p-6 rounded-lg shadow-md">
                            <h2 className="text-xl font-semibold text-gray-800 mb-4">FAILED</h2>
-                           <p className="text-3xl font-bold text-red-600">200</p>
+                           <p className="text-3xl font-bold text-red-600">{count.failed}</p>
                        </div>
                    </div>
 
                    {/* job generator */}
                     <JobInput></JobInput>
 
+                   <div className="flex mt-3 mb-3 gap-2">
+                       <button type="button" onClick={excePendingJob} className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                           Execute Pending Job
+                           <span className="inline-flex items-center justify-center w-10 h-4 ms-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
+                            {count.pending}
+                            </span>
+                       </button>
+                       <button type="button" onClick={rollBackFailedJob} className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                           Roll Back Failed Job
+                           <span className="inline-flex items-center justify-center w-10 h-4 ms-2 text-xs font-semibold text-blue-800 bg-red-200 rounded-full">
+                            {count.failed}
+                            </span>
+                       </button>
+                   </div>
+
                     {/* queue monitor   */}
-                    <QueueMonitor className="mt-3"></QueueMonitor>
+                    <QueueMonitor data={rows}></QueueMonitor>
                </div>
            </div>
        </>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api/': {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api/, '')
+        },
+      }
+    },
+  })
+}
